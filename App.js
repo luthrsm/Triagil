2d9e14da98610c345dac01d0d36e64f4851e9b34
nicolas santos <nicosantosmaia18@gmail.com>
2024-09-12 21:24:29 -0300

    hackaetec finalizado

--- App.js
+++ App.js
@@ -1,20 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+// Telas
+import FormTriagem from './components/atendimentoPessoalTitular';
+import Home from './components/home';
+import TriagemForm from './components/fichaAtendimentoTitular';
+import Carteirinha from './components/carteirinha';
+import PrimeirosSocorros from './components/PrimeirosSocorros';
+import FormTriagemDependente from './components/atendimentoPessoalDependente'
+import scoringSystem from './components/resultado'
+import ResultadoTriagemD from './components/resultadoDependente';
+
+const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
+
+function FormTriagemTitular() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="FormTriagem" component={FormTriagem} />
+      <Stack.Screen name="TriagemForm" component={TriagemForm} />
+      <Stack.Screen name="Resultado" component={scoringSystem} />
+    </Stack.Navigator>
   );
 }
 
+function FormTriagemD() {
+  return (
+    <Stack.Navigator
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="FormTriagem" component={FormTriagemDependente} />
+      <Stack.Screen name="TriagemForm" component={TriagemForm} />
+      <Stack.Screen name="Resultado" component={ResultadoTriagemD} />
+    </Stack.Navigator>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        screenOptions={({ navigation }) => ({
+          headerLeft: () => (
+            <Image source={require('./assets/logo2.png')} style={styles.logo} />
+          ),
+          headerRight: () => (
+            <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
+              <Ionicons
+                name="menu"
+                size={30}
+                color="#fff"
+                style={styles.menuIcon}
+              />
+            </TouchableOpacity>
+          ),
+          headerTitle: '',
+          headerStyle: {
+            backgroundColor: '#6DD9C9',
+          },
+        })}>
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Ficha de Atendimento Titular" component={FormTriagemTitular} />
+        <Drawer.Screen name="Ficha de Atendimento Dependente" component={FormTriagemD} />
+        <Drawer.Screen name="Carteirinha" component={Carteirinha} />
+        <Drawer.Screen name="Primeiros Socorros" component={PrimeirosSocorros} />
+
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  logo: {
+    width: 80,
+    height: 30,
+    marginLeft: -10,
+    resizeMode: 'contain',
+  },
+  menuIcon: {
+    marginRight: 15,
   },
 });
